4cae2e683ee5fca994a165e207bf36dd964a680a
Achilleas Eftychiou <AEftychiou1@outlook.com>
2024-10-10 23:51:45 +0300

    fixes on graph

--- app/index.tsx
+++ app/index.tsx
@@ -6,24 +6,27 @@ import {
   Input,
   Layout,
   Text,
-  useTheme,
 } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import React, { useState } from "react";
-import { Alert, Platform, View } from "react-native";
-import CircularProgress from "react-native-circular-progress-indicator"; // 180-degree circular progress
+import { Alert, Dimensions, Platform, ScrollView, View } from "react-native";
+import CircularProgress from "react-native-circular-progress-indicator";
 
 export default function App() {
   const [weight, setWeight] = useState<string>("");
   const [height, setHeight] = useState<string>("");
+  const [age, setAge] = useState<string>("");
   const [bmi, setBmi] = useState<number | null>(null);
   const [bmiCategory, setBmiCategory] = useState<string>("");
-  const theme = useTheme();
 
-  const MIN_WEIGHT = 30;
-  const MAX_WEIGHT = 300;
-  const MIN_HEIGHT = 100;
-  const MAX_HEIGHT = 250;
+  const screenWidth = Dimensions.get("window").width;
+
+  const MIN_WEIGHT = 60;
+  const MAX_WEIGHT = 660;
+  const MIN_HEIGHT = 36;
+  const MAX_HEIGHT = 100;
+  const MIN_AGE = 18;
+  const MAX_AGE = 100;
 
   const showAlert = (title: string, message: string) => {
     if (Platform.OS === "web") {
@@ -36,27 +39,31 @@ export default function App() {
   const calculateBMI = () => {
     const weightValue = parseFloat(weight);
     const heightValue = parseFloat(height);
+    const ageValue = parseFloat(age);
 
-    if (!validateInputs(weightValue, heightValue)) {
+    if (!validateInputs(weightValue, heightValue, ageValue)) {
       return;
     }
 
-    const heightInMeters = heightValue / 100;
-    const bmiValue = weightValue / (heightInMeters * heightInMeters);
+    const bmiValue = (weightValue / (heightValue * heightValue)) * 703;
     setBmi(parseFloat(bmiValue.toFixed(2)));
     classifyBMI(bmiValue);
   };
 
-  const validateInputs = (weightValue: number, heightValue: number) => {
-    if (!weightValue || !heightValue) {
-      showAlert("Input Error", "Please enter both weight and height.");
+  const validateInputs = (
+    weightValue: number,
+    heightValue: number,
+    ageValue: number
+  ) => {
+    if (!weightValue || !heightValue || !ageValue) {
+      showAlert("Input Error", "Please enter weight, height, and age.");
       return false;
     }
 
     if (weightValue < MIN_WEIGHT || weightValue > MAX_WEIGHT) {
       showAlert(
         "Input Error",
-        `Weight should be between ${MIN_WEIGHT} kg and ${MAX_WEIGHT} kg.`
+        `Weight should be between ${MIN_WEIGHT} lbs and ${MAX_WEIGHT} lbs.`
       );
       return false;
     }
@@ -64,7 +71,15 @@ export default function App() {
     if (heightValue < MIN_HEIGHT || heightValue > MAX_HEIGHT) {
       showAlert(
         "Input Error",
-        `Height should be between ${MIN_HEIGHT} cm and ${MAX_HEIGHT} cm.`
+        `Height should be between ${MIN_HEIGHT} inches and ${MAX_HEIGHT} inches.`
+      );
+      return false;
+    }
+
+    if (ageValue < MIN_AGE || ageValue > MAX_AGE) {
+      showAlert(
+        "Input Error",
+        `Age should be between ${MIN_AGE} and ${MAX_AGE} years.`
       );
       return false;
     }
@@ -87,6 +102,7 @@ export default function App() {
   const resetForm = () => {
     setWeight("");
     setHeight("");
+    setAge("");
     setBmi(null);
     setBmiCategory("");
   };
@@ -94,139 +110,186 @@ export default function App() {
   const getProgressColor = () => {
     switch (bmiCategory) {
       case "Underweight":
-        return theme["color-info-500"];
+        return "#17a2b8"; // Hex code for a blue color
       case "Normal":
-        return theme["color-success-500"];
+        return "#28a745"; // Hex code for a green color
       case "Overweight":
-        return theme["color-warning-500"];
+        return "#ffc107"; // Hex code for a yellow color
       case "Obese":
-        return theme["color-danger-500"];
+        return "#dc3545"; // Hex code for a red color
       default:
-        return "#28a745"; // Default color for progress
+        return "#6c757d"; // Default gray color
     }
   };
 
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
       <IconRegistry icons={EvaIconsPack} />
-      <Layout
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          paddingHorizontal: 20,
-          paddingVertical: 40,
-        }}
-      >
-        <Text category="h1">BMI Calculator</Text>
-
-        <Input
-          style={{ marginTop: 20, width: "20%" }} // Reduce width for better layout
-          placeholder="Enter your weight (kg)"
-          value={weight}
-          inputMode="numeric"
-          onChangeText={setWeight}
-          status={
-            weight &&
-            (parseFloat(weight) < MIN_WEIGHT || parseFloat(weight) > MAX_WEIGHT)
-              ? "danger"
-              : "basic"
-          }
-        />
-        {weight &&
-          (parseFloat(weight) < MIN_WEIGHT ||
-            parseFloat(weight) > MAX_WEIGHT) && (
-            <Text status="danger" category="c1">
-              Weight must be between {MIN_WEIGHT} and {MAX_WEIGHT} kg.
-            </Text>
-          )}
-
-        <Input
-          style={{ marginTop: 20, width: "20%" }} // Reduce width for better layout
-          placeholder="Enter your height (cm)"
-          value={height}
-          inputMode="numeric"
-          onChangeText={setHeight}
-          status={
-            height &&
-            (parseFloat(height) < MIN_HEIGHT || parseFloat(height) > MAX_HEIGHT)
-              ? "danger"
-              : "basic"
-          }
-        />
-        {height &&
-          (parseFloat(height) < MIN_HEIGHT ||
-            parseFloat(height) > MAX_HEIGHT) && (
-            <Text status="danger" category="c1">
-              Height must be between {MIN_HEIGHT} and {MAX_HEIGHT} cm.
-            </Text>
-          )}
-
-        <Button
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Layout
           style={{
-            marginTop: 20,
-            width: "20%",
-            borderRadius: 8,
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            paddingHorizontal: 20,
+            paddingVertical: 40,
+            width: "100%",
           }}
-          onPress={calculateBMI}
         >
-          Calculate BMI
-        </Button>
+          <Text category="h1" style={{ fontSize: screenWidth > 600 ? 32 : 28 }}>
+            BMI Calculator
+          </Text>
 
-        {bmi !== null && (
-          <>
-            <Button
-              style={{ marginTop: 10, width: "20%", borderRadius: 8 }}
-              onPress={resetForm}
-              status="basic"
+          {/* Input fields */}
+          <View
+            style={{ width: screenWidth > 600 ? "40%" : "90%", marginTop: 20 }}
+          >
+            <Text
+              category="label"
+              style={{ fontSize: screenWidth > 600 ? 18 : 16 }}
             >
-              Try Again
-            </Button>
+              Weight (lbs)
+            </Text>
+            <Input
+              placeholder="Enter your weight (lbs)"
+              value={weight}
+              inputMode="numeric"
+              onChangeText={setWeight}
+              status={
+                weight &&
+                (parseFloat(weight) < MIN_WEIGHT ||
+                  parseFloat(weight) > MAX_WEIGHT)
+                  ? "danger"
+                  : "basic"
+              }
+              textStyle={{ fontSize: screenWidth > 600 ? 18 : 16 }} // Responsive font size
+            />
+          </View>
 
-            <View style={{ marginTop: 30, alignItems: "center" }}>
-              <Text category="h4" style={{ fontSize: 26, fontWeight: "bold" }}>
-                Your BMI is: {bmi}
-              </Text>
-              <Text
-                category="h5"
+          <View
+            style={{ width: screenWidth > 600 ? "40%" : "90%", marginTop: 20 }}
+          >
+            <Text
+              category="label"
+              style={{ fontSize: screenWidth > 600 ? 18 : 16 }}
+            >
+              Height (inches)
+            </Text>
+            <Input
+              placeholder="Enter your height (inches)"
+              value={height}
+              inputMode="numeric"
+              onChangeText={setHeight}
+              status={
+                height &&
+                (parseFloat(height) < MIN_HEIGHT ||
+                  parseFloat(height) > MAX_HEIGHT)
+                  ? "danger"
+                  : "basic"
+              }
+              textStyle={{ fontSize: screenWidth > 600 ? 18 : 16 }} // Responsive font size
+            />
+          </View>
+
+          <View
+            style={{ width: screenWidth > 600 ? "40%" : "90%", marginTop: 20 }}
+          >
+            <Text
+              category="label"
+              style={{ fontSize: screenWidth > 600 ? 18 : 16 }}
+            >
+              Age (years)
+            </Text>
+            <Input
+              placeholder="Enter your age (years)"
+              value={age}
+              inputMode="numeric"
+              onChangeText={setAge}
+              status={
+                age && (parseFloat(age) < MIN_AGE || parseFloat(age) > MAX_AGE)
+                  ? "danger"
+                  : "basic"
+              }
+              textStyle={{ fontSize: screenWidth > 600 ? 18 : 16 }} // Responsive font size
+            />
+          </View>
+
+          {/* Calculate BMI button */}
+          <Button
+            style={{
+              marginTop: 30,
+              width: screenWidth > 600 ? "40%" : "90%",
+              borderRadius: 8,
+              paddingVertical: 15, // Adjust for better button size
+            }}
+            onPress={calculateBMI}
+          >
+            Calculate BMI
+          </Button>
+
+          {/* Results */}
+          {bmi !== null && (
+            <>
+              <Button
                 style={{
-                  fontSize: 20,
-                  fontWeight: "bold",
-                  color:
-                    bmiCategory === "Underweight"
-                      ? theme["color-info-500"]
-                      : bmiCategory === "Normal"
-                      ? theme["color-success-500"]
-                      : bmiCategory === "Overweight"
-                      ? theme["color-warning-500"]
-                      : theme["color-danger-500"],
+                  marginTop: 20,
+                  width: screenWidth > 600 ? "40%" : "90%",
+                  borderRadius: 8,
+                  paddingVertical: 15, // Adjust for better button size
                 }}
+                onPress={resetForm}
+                status="basic"
               >
-                {bmiCategory}
-              </Text>
+                Try Again
+              </Button>
 
-              <CircularProgress
-                value={bmi}
-                radius={100}
-                duration={2500} // Increased duration for smoother transition
-                activeStrokeWidth={12}
-                inActiveStrokeWidth={12}
-                inActiveStrokeOpacity={0.2}
-                activeStrokeColor={getProgressColor()} // Dynamic color based on BMI category
-                inActiveStrokeColor="#cccccc"
-                strokeLinecap="round"
-                maxValue={40} // Set the maximum value of the BMI range
-                titleColor="#000"
-                titleStyle={{ fontWeight: "bold", fontSize: 24 }} // Adjust font size
-                title="" // Remove title to prevent duplicate value
-                valueSuffix="" // Remove the suffix to avoid showing extra value
-                rotation={-90} // Start from the bottom of the circle (180 degrees)
-                clockwise={false} // Ensure it behaves like a 180-degree gauge
-              />
-            </View>
-          </>
-        )}
-      </Layout>
+              <View style={{ marginTop: 30, alignItems: "center" }}>
+                <Text
+                  category="h4"
+                  style={{
+                    fontSize: screenWidth > 600 ? 26 : 22,
+                    fontWeight: "bold",
+                  }}
+                >
+                  Your BMI is: {bmi}
+                </Text>
+
+                {/* Space between text and circle */}
+                <Text
+                  category="h5"
+                  style={{
+                    fontSize: screenWidth > 600 ? 20 : 18,
+                    fontWeight: "bold",
+                    marginBottom: 15,
+                    color: getProgressColor(),
+                  }}
+                >
+                  {bmiCategory}
+                </Text>
+
+                <CircularProgress
+                  value={bmi}
+                  radius={screenWidth > 600 ? 100 : 80} // Adjust circle size for screen width
+                  duration={2500}
+                  activeStrokeWidth={12}
+                  inActiveStrokeWidth={12}
+                  inActiveStrokeOpacity={0.2}
+                  activeStrokeColor={getProgressColor()} // Dynamic color based on BMI category
+                  inActiveStrokeColor="#cccccc"
+                  strokeLinecap="round"
+                  maxValue={40} // Max BMI value
+                  titleColor="#000"
+                  titleStyle={{ fontWeight: "bold", fontSize: 24 }}
+                  title=""
+                  valueSuffix=""
+                  rotation={0} // Start from top (0 degrees)
+                  clockwise={true} // Rotate clockwise for full 360 degrees
+                />
+              </View>
+            </>
+          )}
+        </Layout>
+      </ScrollView>
     </ApplicationProvider>
   );
 }
